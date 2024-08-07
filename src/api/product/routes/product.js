0183f2d7c623fa5e8c113eaefb95523a7d5996f9
=vijaysathviab <vijaysathvikab@gmail.com>
2024-08-07 10:00:00 +0530

    feat(api): customize product routes

---
+++ src/api/product/routes/product.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * product router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::product.product', {
+  config: {
+    find: {
+      auth: false, // Public access
+    },
+    findOne: {
+      auth: false,
+    },
+  },
+});
